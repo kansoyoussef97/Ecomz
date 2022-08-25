dc409f4bdb05be50987f1ea79aef7cf728db9e6b
youssef-k <youssef.k@totersapp.com>
2022-08-25 12:11:16 +0300

    remove unused lines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  rootURL = `https://62fb9cb8abd610251c0d7310.mockapi.io/api/v1/cab`;
-  title = 'project';
 
   constructor() {}
 
